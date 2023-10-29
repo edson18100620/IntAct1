e6329d7a2eb260bce77c809c49ad11564e9c5190
Edson <18100620@ue.edu.pe>
2023-10-29 15:45:30 -0500

    List View practica

--- app/src/main/java/com/example/intact1/ResultActivity.kt
+++ app/src/main/java/com/example/intact1/ResultActivity.kt
@@ -3,19 +3,23 @@ package com.example.intact1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListAdapter
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
 
-    var arrSo = arrayOf(
+    var arrSO = arrayOf(
         "Android", "iPhone", "Windows Phone", "Blackberry", "Firefox OS", "Amazon World", "Nokia"
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        /*val message = intent.getStringExtra("param")
+        val message = intent.getStringExtra("param")
         val tvResult: TextView = findViewById(R.id.tvResult)
         tvResult.text = message
 
@@ -23,7 +27,19 @@ class ResultActivity : AppCompatActivity() {
         btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }*/
-        
+        }
+
+        val lsvSistemasOperativos: ListView = findViewById(R.id.lsvSO)
+        val adaptador: Any? = ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1,arrSO)
+
+        lsvSistemasOperativos.adapter = adaptador as ListAdapter?
+
+        lsvSistemasOperativos.setOnItemClickListener { parent, view, position, id ->
+            println("Aqui mostraré un mensaje al usuario")
+            Toast.makeText(
+                applicationContext,"La posicion seleccionada es: $position", Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
